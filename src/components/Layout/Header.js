fbc3110c09d4599b31593689b905d34aa5dadbf0
Ananya Sharma <ananyasubodh8@gmail.com>
2021-12-09 20:02:58 +0530

    StaticDesignDone

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCardButton from './HeaderCardButton'
+
+function Header({props}) {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="table-full-of-food"/>
+      </div>
+    </>
+  )
+}
+
+export default Header
